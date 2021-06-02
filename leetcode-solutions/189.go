15d58f5e803a6d4c0b060541fc61815844e046cd
laststem <laststem@gmail.com>
2021-06-03 01:34:20 +0900

    Create 189.go

---
+++ leetcode-solutions/189.go
@@ -0,0 +1,15 @@
+func rotate(nums []int, k int)  {
+    l := len(nums)
+    k = k % l
+    reverse(nums, 0, l-1)
+    reverse(nums, 0, k-1)
+    reverse(nums, k, l-1)
+}
+
+func reverse(nums []int, start, end int) {
+    for start < end {
+        nums[start], nums[end] = nums[end], nums[start]
+        start++
+        end--
+    }
+}
