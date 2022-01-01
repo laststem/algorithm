9219e0f4b9ec3e197e08beee47a103db35ab31d6
laststem <laststem@gmail.com>
2022-01-01 18:23:13 +0900

    Create 515.cpp

---
+++ leetcode-solutions/515.cpp
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> largestValues(TreeNode* root) {
+        vector<int> answer;
+        if (!root) {
+            return answer;
+        }
+        queue<TreeNode*> q;
+        q.push(root);
+        while(!q.empty()) {
+            int size = q.size();
+            int m = INT_MIN;
+            for (int i=0; i<size; i++) {
+                TreeNode* node = q.front();
+                q.pop();
+                if (m < node->val) {
+                    m = node->val;
+                }
+                if (node->left != nullptr) {
+                    q.push(node->left);
+                }
+                if (node->right != nullptr) {
+                    q.push(node->right);
+                }
+            }
+            answer.push_back(m);
+        }
+        return answer;
+    }
+};
